dc569b8706b6fc7932ccb42e813df8bd57cda5a0
ShinHeejo <shinheejo@gmail.com>
2025-11-21 18:10:00 +0900

    스프링 프로젝트입니다

---
+++ CardGameSpring/src/main/java/kr/ac/ewha/java2/CardGameSpringApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.ewha.java2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardGameSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardGameSpringApplication.class, args);
+	}
+
+}
