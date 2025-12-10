14eb22d0563b15b3a4b1c3b685976e23c0772159
ShinHeejo <shinheejo@gmail.com>
2025-12-10 15:18:57 +0900

    Delete JavaTeamProjcet_8 directory

--- JavaTeamProjcet_8/src/domain/card/Card.java
+++
@@ -1,8 +0,0 @@
-package domain.card;
-
-public interface Card {
-    int getNumber();      // 카드의 기본 숫자
-    int getPower();       // 전투력 (버프 적용됨)
-    int getScore();       // 승리 시 획득 점수 (버프 적용됨)
-    String getName();     // 카드 이름 (예: "⚔️공격형 [7]")
-}
